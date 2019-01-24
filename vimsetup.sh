011d31894932ccc95b61aade07b3b531d240efc2
Dale Martin <dalemartin89@gmail.com>
2019-01-24 00:50:30 -0500

    Fixed recursive .vim symlink in vimsetup.sh

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Run script from home directory
+cd ~
+
 # Set symbolic links to vim dotfiles
-ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.viminfo ~/.viminfo
+if [ -L .vim ]; then
+    # remove existing symlink to .vim to avoid recursive linking
+    rm .vim
+fi
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
 
 # Install Vundle handled plugins
 vim +PluginInstall +qall
+
+# Restore directory
+cd -
